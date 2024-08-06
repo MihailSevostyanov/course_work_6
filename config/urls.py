16f01678721f0cb797092763be3a513747e8f3b0
Mihail Sevostyanov <sevamihael141095@gmail.com>
2024-08-06 16:51:05 +0300

    Добавление модели пользователя

--- config/urls.py
+++ config/urls.py
@@ -6,4 +6,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mailing.urls', 'mailing')),
+    path('users/', include('users.urls', 'users')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
